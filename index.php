6274ac94413dab2cb45e11269e7850ae472a181a
lamre <lamryan@live.com>
2015-03-24 16:43:09 -0700

    Index page updated for database connection

--- index.php
+++ index.php
@@ -5,5 +5,19 @@
         ?>
         This is a test.
         <br><a href="login.php">Try clicking this</a>
+        
+        <?php
+        //mysqli('host', 'user', 'password', 'database');
+        $db = new mysqli('zeeveener.com', 'collier', 'rox', 'collier');
+        
+        if ($db->connect_errno > 0)
+        {
+            die('Unable to connect to database [' . $db->connect_error . ']');
+        }
+        else
+        {
+            print('Database Connected');
+        }    
+        ?>
     </body>
 </html>
