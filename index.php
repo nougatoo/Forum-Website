3b0cdca8e201d15841d8828213f0d3a381c16fee
lamre <lamryan@live.com>
2015-04-01 12:25:24 -0700

    Showing Forum Perm, Board Perm kinda thing. Aka like, if you're an admin, you should have the I CAN CREATE FORUM & CREATE BOARD text displayed, whereas as a user, you don't have those displayed.

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
+<?php
+    require 'functions.php';
+?>        
+
 <html>
     <body>
-        <?php
-            print("Hello World, just testing -Brandon");
-            require 'functions.php';
-        ?>        
+
         <?php if (isset($_SESSION['username'])) : ?>
             We are set set.
             <br><a href='logout.php'>Log Out</a>
@@ -20,25 +21,38 @@
             if (isset($_SESSION['username']))
             {
                 $user = $_SESSION['username'];
-                $query = "SELECT create_forum FROM rank JOIN user ON user.rank=rank.id WHERE username='$user'";
-                $result = mysqli_query($db, $query);
+                $query = "SELECT create_forum, create_board FROM rank JOIN user ON user.rank=rank.id WHERE username='$user'";
+                $perm_result = mysqli_query($db, $query);
                 
-                if ($result->num_rows>0)
+                if ($perm_result->num_rows>0)
                 {
-                    $value = $result->fetch_assoc();
+                    $value = $perm_result->fetch_assoc();
                     if ($value['create_forum']==1)
                     {
-                        echo 'I am an Administrator! I have Rights!!';
+                        echo 'I am an Administrator! I have Rights to Create Forums!!<br>';
+                        /*
+                         * TODO: Display "Add Forum" Function.
+                         * Also display the Forum with the corresponding Boards underneath
+                         * We also need to implement the "Add Board" Function underneath every forum.
+                         * Possibly assign the Forum ID to be associated with the Add Board link?
+                         * Extra: Delete Forum
+                         */
                     }
-                    else
+                    
+                    if ($value['create_board']==1)
                     {
-                        echo 'Lol I anit an admin...';
+                        echo 'I have permission to create boards!<br>';
+                        
                     }
                 }
                 else
                 {
                     echo "Welp this didnt work";
                 }
+                /*
+                * TODO: Display Forums for Regular Users
+                * We may not even need this else statement, because Admins should be able to see the rest of the Forums too.
+                */
             }
         ?>
            <!-- Need to join the user with rank, and then check for create_forum
