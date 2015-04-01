3e79b6a30a1b8738d38136636a7c2674cb2202f4
lamre <lamryan@live.com>
2015-03-31 23:45:05 -0700

    Fixed Register Date

--- register.php
+++ register.php
@@ -14,16 +14,19 @@
             $email = $_POST['email'];
             $title = $_POST['title'];
             $gender = $_POST['gender'];
+            $date = date("Y-m-d");
             $rank = 1;
+            
+            echo "Date is $date";
 
             print($username);
             //Only put in one check for the sake of time, will add more after
             if (!empty($username)) {
-                $query = "INSERT INTO `user` (username,signature,email,gender,title,password,rank) VALUES ('$username', '$signature', '$email', '$gender', '$title', '$password', $rank)";
+                $query = "INSERT INTO `user` (username,signature,email,gender,title,password,datejoined,rank) VALUES ('$username', '$signature', '$email', '$gender', '$title', '$password', '$date', $rank)";
                 $result = mysqli_query($db, $query);
                 if($result)
                 {
-                    header("Location: login.php");
+                    header("refresh:5; url=login.php");
                 }
                 else
                 {
