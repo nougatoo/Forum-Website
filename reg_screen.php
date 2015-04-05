a093c35b6f03200baf1bc9c9a692f05065af336e
Cain Abaddon <ajr.zimmer@gmail.com>
2015-04-05 16:50:45 -0600

    dropdown selector for gender. Should we have age restriction?

--- reg_screen.php
+++ reg_screen.php
@@ -1,25 +1,31 @@
 
 <html>
-    <head>
+<head>
     <script>
         function gotoregister() {
-    
             location.href = "register.php"
         }
-        
+
     </script>
-    </head>
-    
-    <body>
-        
-        <form action="register.php" method="post">
-        Username: <input type="text" name="username_r"><br>
-        Password: <input type="text" name="password_r"><br>
-        Signature: <input type="text" name="signature"><br>
-        Email Address: <input type="text" name="email"><br>
-        Gender: <input type="text" name="gender"><br>
-        Title: <input type="text" name="title"><br>
-        <input type="submit" value="Register">
-        
-    </body>
+</head>
+
+<body>
+
+<form action="register.php" method="POST">
+    Username: <input type="text" name="username_r"><br>
+    Password: <input type="password" name="password_r"><br>
+    Signature: <input type="text" name="signature"><br>
+    Email Address: <input type="text" name="email"><br>
+    Gender: <select name="gender">
+        <option disabled="disabled" name="gender" value="">Choose one:</option>
+        <option name="gender" value="female">Female</option>
+        <option name="gender" value="male">Male</option>
+        <option name="gender" value="other">Other</option>
+    </select><br>
+    <!-- Do we want to have the user's age? for age restrictions to the site?-->
+    Age: <input type="text" name="age"><br>
+    Title: <input type="text" name="title"><br>
+    <input type="submit" value="Register">
+
+</body>
 </html> 
--- register.php
+++ register.php
@@ -1,48 +1,42 @@
 <?php
-    require 'functions.php';
-    initialize();
-?>      
+require 'functions.php';
+initialize();
+?>
 <html>
-    <body>
-        <?php
+<body>
+<?php
 
-        if (isset($_POST['username_r']) && isset($_POST['password_r']) && isset($_POST['signature']) && isset($_POST['email']) && isset($_POST['title']) && isset($_POST['gender']))
+if (isset($_POST['username_r']) && isset($_POST['password_r']) && isset($_POST['signature']) && isset($_POST['email']) && isset($_POST['title']) && isset($_POST['gender']))
+{
+    $username = $_POST['username_r'];
+    $password = $_POST['password_r'];
+    $signature = $_POST['signature'];
+    $email = $_POST['email'];
+    $title = $_POST['title'];
+    $gender = $_POST['gender'];
+    $date = date("Y-m-d");
+    $rank = 1;
+
+    echo "Date is $date\n";
+    echo "Username is $username";
+    //Only put in one check for the sake of time, will add more after
+    if (!empty($username)) {
+        $query = "INSERT INTO `user` (username,signature,email,gender,title,password,datejoined,rank) VALUES ('$username', '$signature', '$email', '$gender', '$title', '$password', '$date', $rank)";
+        $result = mysqli_query($db, $query);
+        if($result)
         {
-
-            $username = $_POST['username_r'];
-            $password = $_POST['password_r'];
-            $signature = $_POST['signature'];
-            $email = $_POST['email'];
-            $title = $_POST['title'];
-            $gender = $_POST['gender'];
-            $date = date("Y-m-d");
-            $rank = 1;
-            
-            echo "Date is $date";
-
-            print($username);
-            //Only put in one check for the sake of time, will add more after
-            if (!empty($username)) {
-                $query = "INSERT INTO `user` (username,signature,email,gender,title,password,datejoined,rank) VALUES ('$username', '$signature', '$email', '$gender', '$title', '$password', '$date', $rank)";
-                $result = mysqli_query($db, $query);
-                if($result)
-                {
-                    header("refresh:5; url=login.php");
-                }
-                else
-                {
-                    print("<br> Registration was not successful, please go back and try again");
-                }
-            }
+            header("refresh:5; url=login.php");
+        } else {
+            print("<br> Registration was not successful, please go back and try again");
         }
-        else
-        {
-            print("<br>I'm not working");
-        }
+    }
+} else {
+    print("<br>I'm not working");
+}
 
-        ?>
-        <br>
-        <br>
-        <a href="index.php">Go Back</a>
-    </body>
+?>
+<br>
+<br>
+<a href="index.php">Go Back</a>
+</body>
 </html>
