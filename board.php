019fadda44c30e480070dfd88824a62cab0f86e6
Brandon <Brandon@Nougatoo>
2015-04-07 12:47:37 -0600

    Users can now view topics and the posts that have been posted in reply to those topics

--- board.php
+++ board.php
@@ -12,9 +12,65 @@ $_SESSION['bid'] = $_POST['goto_board'];
 print("This Board's ID# is: ");
 print($_SESSION['bid']);
 $bid = $_SESSION['bid'];
+
+//Going to get all the topics that belong to this board
+$query = "SELECT id, title, noreply, datecreated, views, sticky, hidden, boardid, username FROM topic WHERE boardid=$bid";
+$result = mysqli_query($db, $query);
+
+echo "<br>"."<br>"."Stickied Topics"."<br>"."<br>";
+
+
+//This will show all the sticked topics first
+while($row = $result->fetch_assoc()){
+    
+    if($row['sticky'] == 1 && $row['hidden'] != 1)
+    {
+        echo "".($row['title'])."<br>";
+        echo "Number of replies: ".($row['noreply'])."<br>";
+        echo "Date Created: ".($row['datecreated'])."<br>";
+        echo "Created by: ".($row['username'])."<br>";
+        ?>
+
+        <form action = "topic.php" method = "post">
+            <input type="hidden" value="<?php echo $row['id']?>" name="tid">
+            <input type="submit" value="See Topic" />
+        </form>
+
+        <?php
+        echo "<br>"."<br>";
+    }
+}
+
+//This will show the rest of the topics that aren't stickied
+$result = mysqli_query($db, $query);
+while($row = $result->fetch_assoc()){
+    
+    if($row['hidden'] != 1)
+    {
+        echo "".($row['title'])."<br>";
+        echo "Number of replies: ".($row['noreply'])."<br>";
+        echo "Date Created: ".($row['datecreated'])."<br>";
+        echo "Created by: ".($row['username'])."<br>";
+        
+        ?>
+
+        <form action = "topic.php" method = "post">
+            <input type="hidden" value="<?php echo $row['id']?>" name="tid">
+            <input type="submit" value="See Topic" />
+        </form>
+
+        <?php
+        echo "<br>"."<br>";
+    } 
+}
+
+
+
 //Now we need to retrieve the board title and description from the DB as a test
 $query = "SELECT title, description FROM board WHERE id=$bid";
 $result = mysqli_query($db, $query);
+
+
 while ($row = $result->fetch_assoc()){
     ?>
     <br>
