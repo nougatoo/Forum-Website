d2c9041537bd3f6f4262a106fca3ac33536caee4
Cain Abaddon <ajr.zimmer@gmail.com>
2015-04-08 18:01:34 -0600

    Trying to make the bio editable

--- profile_page.php
+++ profile_page.php
@@ -8,7 +8,7 @@ $username = $_SESSION['username'];
 print($username);
 print("'s Profile Page");
 
-$query = "SELECT rank, signature, email FROM user WHERE username = '$username'";
+$query = "SELECT * FROM user WHERE username = '$username'";
 $result = mysqli_query($db, $query);
 while($row = $result->fetch_assoc()) {
     $rank = $row['rank'];
@@ -16,7 +16,6 @@ while($row = $result->fetch_assoc()) {
     $email = $row['email'];
     $rankColour = "black";
     $rankMsg = "Default Message";
-
     switch ($rank) {
         case 0:
             $rankMsg = "You are an Administrator";
@@ -38,11 +37,33 @@ while($row = $result->fetch_assoc()) {
             echo "Error: Unknown Rank";
     }
     ?>
-    <br> Rank: <p><span style="color: <?php echo $rankColour?>; "><?php echo $rankMsg?></span></p>
-    Signature: <?php echo $row['signature']?>
+    <br> Rank: <span style="color: <?php echo $rankColour?>; "><?php echo $rankMsg?></span>
+    <br> Title: <?php echo $row['title']?>
+    <br> Signature: <?php echo $row['signature']?>
+    <br> Email: <?php echo $row['email']?>
+    <br> Gender: <?php echo $row['gender']?>
+    <br> Date Joined: <?php echo $row['datejoined']?>
 <?php
 }
 ?>
+    <br> Biography: <br>
+    <textarea name="user_bio" rows="10" cols="50" maxlength="500" placeholder="My Biography" readonly></textarea> <br>
+    <input name="Edit" type="button" value="Update Bio" onclick="edit_text()">
+
+<!-- trying to figure out how toggle the readonly attribute of the textblock -->
+<script>
+    function edit_text(){
+
+    }
+</script>
+
+<!-- Go back function if we want the edit profile link on every page-->
+<br> <button onclick="goBack()">Exit</button>
+<script>
+    function goBack(){
+        window.history.back();
+    }
+</script>
 
 </body>
 </html>
