1a07b8816093e8783ff2c286a9d47b9fffac7459
zeeveener <zeeveener@gmail.com>
2015-04-20 22:16:31 -0600

    Made Index use a Table for layout

--- index.php
+++ index.php
@@ -17,13 +17,13 @@
         $user = $_SESSION['username'];
         $query = "SELECT create_forum, create_board FROM rank JOIN user ON user.rank=rank.id WHERE username='$user'";
         $perm_result = mysqli_query($db, $query);
-        if ($perm_result->num_rows>0){
+        if ($perm_result->num_rows > 0) {
             $value = $perm_result->fetch_assoc();
-            if ($value['create_forum']==1){
+            if ($value['create_forum'] == 1) {
 ?>
 
-        <form action = "create_forum_screen.php" method = "get">
-            <input type="submit" value="Create Forum" />
+        <form action="create_forum_screen.php" method="get">
+            <input type="submit" value="Create Forum"/>
         </form>
 
 <?php
@@ -40,40 +40,47 @@
             . " AND rankid IN (SELECT rank FROM user WHERE username='$user'));";
         $result = mysqli_query($db, $query);
 
-        while($row = $result->fetch_assoc()){
-            $fid = $row["id"];
 ?>
-            <br>
-            <span>
-                <h2><?php print($row["title"])?>:</h2>
+        <table cellspacing="2" cellpadding="2" align="center">
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+            </tr>
 
-<?php
-            if ($create_board == true){
-?>
-                <form action = "create_board_screen.php" method = "get">
-                    <button type="submit" name="add_board" value="<?php echo $fid?>"> Add Board</button>
-                </form>
-<?php
+            <?php
+            while ($row = $result->fetch_assoc()) {
+                $fid = $row["id"];
+                ?>
+                <tr>
+                    <th class="topicTypeHeader" colspan="20">
+                        <?php echo $row["title"]?>
+                        <?php
+                        if ($create_board == true) {
+                            ?>
+                            <a href="create_board_screen.php?add_board=<?php echo $fid?>">Create New Board</a>
+                        <?php
+                        }
+                        ?>
+                    </th>
+                </tr>
+                <?php
+                $query2 = "SELECT title, description,id FROM board WHERE forumid=$fid";
+                $result2 = mysqli_query($db, $query2);
+
+                while ($row2 = $result2->fetch_assoc()) {
+                    $bid = $row2["id"];
+                    ?>
+                    <tr>
+                        <td><a href="board.php?boardid=<?php echo $bid?>"><?php echo $row2["title"]?></a></td>
+                        <td style="text-align: right"><?php echo $row2["description"]?></td>
+                    </tr>
+                <?php
+
+                }
             }
 ?>
-            </span>
+        </table>
 <?php
-
-            $fid = $row["id"];
-
-            $query2 = "SELECT title, description,id FROM board WHERE forumid=$fid";
-            $result2 = mysqli_query($db, $query2);
-
-            while($row2 = $result2->fetch_assoc()){
-                $bid = $row2["id"];
-?>
-                <form action = "board.php" method = "get">
-                    <h3><?php echo $row2["title"] . ": "?></h3><?php echo $row2["description"]?>
-                    <button type="submit" name="boardid" value="<?php echo $bid?>">Go Here</button>
-                </form>
-<?php
-            }
-        }
     }
 ?>
     </body>
